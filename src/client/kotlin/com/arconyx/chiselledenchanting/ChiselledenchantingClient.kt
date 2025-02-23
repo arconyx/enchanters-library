decfa208dfb7bd9d731ad0b5f3fa484fdfa1da73
ArcOnyx <1ryanc100@gmail.com>
2025-02-24 12:18:59 +1300

    fabric template

---
+++ src/client/kotlin/com/arconyx/chiselledenchanting/ChiselledenchantingClient.kt
@@ -0,0 +1,9 @@
+package com.arconyx.chiselledenchanting
+
+import net.fabricmc.api.ClientModInitializer
+
+object ChiselledenchantingClient : ClientModInitializer {
+	override fun onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
